094e188e2a32cc07967420e255084682e4a25f26
jmc27 <jmc27@brandeis.edu>
2014-01-17 12:48:19 -0500

    Clean up
    strip was removing tabs between data attributes. added testing code at
    the bottom. Currently prints all the movie data

--- movie_data.rb
+++ movie_data.rb
@@ -1,13 +1,22 @@
 
 class MovieData
+	attr_accessor :filename, :data
 	def load_data(filename)
 		@filename = filename
 		@data = {}
 		File.open(filename, 'r').each_line do |line|
-			line = line.strip.split '\t'
+			line = line.split '\t'
 			@data[line.first.to_s] = line.last.to_s
 		end
 	end
 	#print each element
-	data.each { |a| print a, "\n"}
+	def data_to_s
+		@data.each { |a| puts a}
+	end
 end
+
+
+#test
+data = MovieData.new()
+data.load_data("u.data")
+data.data_to_s
